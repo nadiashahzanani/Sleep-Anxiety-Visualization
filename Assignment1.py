aff90df866d0374d2e4d1336169a9d26e9799c5f
nadiashahzanani <nadiashahzanani123@gmail.com>
2025-11-02 04:23:12 +0800

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -24,8 +24,6 @@ st.write(
     """
 )
 
-home = st.Page('home.py', title='Home', icon=":material/school:")
-
 page1 = st.Page('Objectives1.py', title='Page 1', icon=":material/bar_chart:")
 
 page2 = st.Page('Objectives2.py', title='Page 2', icon=":material/groups:")
@@ -34,7 +32,7 @@ page3 = st.Page('Objectives3.py', title='Page 3', icon=":material/timeline:")
 
 pg = st.navigation(
         {
-            "Menu": [home, page1, page2, page3]
+            "Menu": [page1, page2, page3]
         }
     )
 
