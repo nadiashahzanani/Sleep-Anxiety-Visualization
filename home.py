9ca41aa3bd9c84233b77db27cb5b57d3bafb838e
nadiashahzanani <nadiashahzanani123@gmail.com>
2025-11-02 04:15:59 +0800

    Create home.py

---
+++ home.py
@@ -0,0 +1,21 @@
+# ------------------------------------------------------------
+# Page Setup
+# ------------------------------------------------------------
+
+# Header title
+st.header("🧠 Time to Think — Sleep, Anxiety and University Start Time")
+
+# Intro paragraph
+st.write(
+    """
+    A scientific visualization will explores on how sleep quality, anxiety levels and preferred class start times are coonnected among students at university.
+    """
+)
+
+# Dataset information
+st.write(
+    """
+    This data comes from Norbury & Evans (2018) published in Mendeley Data (V1).  
+    The study explores psychological and behavioral patterns related to sleep, chronotype, and academic start times that are related to their academic routines and wellbeing.
+    """
+)
