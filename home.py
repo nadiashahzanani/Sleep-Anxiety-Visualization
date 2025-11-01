7b37fd28b2928f5556ce7b891117041d97d2f01a
nadiashahzanani <nadiashahzanani123@gmail.com>
2025-11-01 19:54:15 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Student Survey"
+    page_title="Sleep, Anxiety & Start Time Visualization App"
 )
 
-page1 = st.Page('Objectives1.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
-page2 = st.Page('Objectives2.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
-page3 = st.Page('Objectives3.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+page1 = st.Page('Objectives1.py', title='Distribution and Correlation', icon=":material/school:")
+page2 = st.Page('Objectives2.py', title='Group Comparisons and Chronotype', icon=":material/school:")
+page3 = st.Page('Objectives3.py', title='Preferred Start Time & Correlation Matrix', icon=":material/school:")
 
 pg = st.navigation(
         {
