07f12d93b76045aed98530911b830ddd19c6ad89
nadiashahzanani <nadiashahzanani123@gmail.com>
2025-11-01 21:20:36 +0800

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,8 +1,38 @@
-import streamlit as st
+# ===================================================
+# Sleep, Anxiety & Start Time Visualization Homepage
+# ===================================================
 
-st.set_page_config(
-    page_title="Sleep, Anxiety & Start Time Visualization App"
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+from scipy import stats
+import numpy as np
+
+# ------------------------------------------------------------
+# Page Setup
+# ------------------------------------------------------------
+st.set_page_config(page_title="Sleep, Anxiety & Start Time Visualization", layout="wide")
+
+# Header title
+st.header("🧠 Time to Think — Sleep, Anxiety and University Start Time")
+
+# Intro paragraph
+st.write(
+    """
+    A scientific visualization exploring how **sleep quality**, **trait anxiety**, and **preferred class start times** interact among university students.
+    """
 )
+
+# Dataset information
+st.write(
+    """
+    This dashboard visualizes data from **Norbury & Evans (2018)** published in *Mendeley Data (V1)*.  
+    The study explores psychological and behavioral patterns related to sleep, chronotype, and academic start times.
+    """
+)
+
 
 page1 = st.Page('Objectives1.py', title='Distribution and Correlation', icon=":material/bar_chart:")
 
