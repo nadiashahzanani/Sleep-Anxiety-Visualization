badc98511dc25e5e85f3f18cb618a4d3c3303e9c
nadiashahzanani <nadiashahzanani123@gmail.com>
2025-11-01 21:02:25 +0800

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -4,11 +4,11 @@ st.set_page_config(
     page_title="Sleep, Anxiety & Start Time Visualization App"
 )
 
-page1 = st.Page('Objectives1.py', title='Distribution and Correlation', icon=":material/school:")
+page1 = st.Page('Objectives1.py', title='Distribution and Correlation', icon=":material/bar_chart:")
 
-page2 = st.Page('Objectives2.py', title='Group Comparisons and Chronotype', icon=":material/school:")
+page2 = st.Page('Objectives2.py', title='Group Comparisons and Chronotype', icon=":material/groups:")
 
-page3 = st.Page('Objectives3.py', title='Preferred Start Time & Correlation Matrix', icon=":material/school:")
+page3 = st.Page('Objectives3.py', title='Preferred Start Time & Correlation Matrix', icon=":material/timeline:")
 
 pg = st.navigation(
         {
