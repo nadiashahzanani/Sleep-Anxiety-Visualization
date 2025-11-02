d037fcdbadf398c3026b76b5216d132d3d32f429
nadiashahzanani <nadiashahzanani123@gmail.com>
2025-11-02 16:09:35 +0800

    Update Objectives3.py

--- Objectives3.py
+++ Objectives3.py
@@ -10,6 +10,15 @@ st.title("Objective 3 — Preferred Start Time by Chronotype")
 url = "https://raw.githubusercontent.com/nadiashahzanani/Sleep-Anxiety-Visualization/refs/heads/main/Time_to_think_Norburyy.csv"
 df = pd.read_csv(url)
 
+# Set the title for the Streamlit app
+st.title("Arts Faculty Student Data")
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+
 # Ensure 'Chronotype' column exists
 if 'Chronotype' not in df.columns:
     # Categorize MEQ scores into Chronotypes (adjust thresholds as needed)
